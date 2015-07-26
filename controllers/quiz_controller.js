16ce1c64081a4d3f8a46fafe253b568e4c9ce757
Jose luis Roncero <jroncerohervas@gmail.com>
2015-07-26 21:09:22 +0200

    Preguntas múltiples

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,11 @@
 
 var models = require('../models/models.js');
 
-// GET /quizes/question
+// GET /quizes/:id
 
-exports.question = function(req, res) {
-   models.Quiz.findAll().success(function(quiz){
-   res.render('quizes/question', {pregunta: quiz[0].pregunta});
+exports.show = function(req, res) {
+   models.Quiz.find(req.params.quizId).success(function(quiz){
+   res.render('quizes/show', { quiz: quiz});
 });
 };
 
@@ -14,15 +14,25 @@ exports.author = function(req, res) {
    res.render('author');
 };
 
+// GET /quizes
 
-// GET /quizes/answer
+exports.index = function(req, res) {
+   models.Quiz.findAll().success(function(quizes){
+   res.render('quizes/index.ejs', { quizes: quizes});
+});
+};
+
+
+
+
+// GET /quizes/:id/answer
 
 exports.answer = function(req, res) {
-models.Quiz.findAll().success(function(quiz){
-	if (req.query.respuesta === quiz[0].respuesta){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
+models.Quiz.find(req.params.quizId).success(function(quiz){
+	if (req.query.respuesta === quiz.respuesta){
+		res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto'});
         } else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
         }
 })
 };
