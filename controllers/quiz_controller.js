d5b2eefb91651a32cd65fb9b50b73f5ae1013714
Jose luis Roncero <jroncerohervas@gmail.com>
2015-07-29 19:41:53 +0200

    Preguntas multiples funcions

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -4,7 +4,7 @@ var models = require('../models/models.js');
 // GET /quizes/:id
 
 exports.show = function(req, res) {
-   models.Quiz.find(req.params.quizId).success(function(quiz){
+   models.Quiz.find(req.params.quizId).then(function(quiz){
    res.render('quizes/show', { quiz: quiz});
 });
 };
@@ -17,7 +17,7 @@ exports.author = function(req, res) {
 // GET /quizes
 
 exports.index = function(req, res) {
-   models.Quiz.findAll().success(function(quizes){
+   models.Quiz.findAll().then(function(quizes){
    res.render('quizes/index.ejs', { quizes: quizes});
 });
 };
@@ -28,11 +28,11 @@ exports.index = function(req, res) {
 // GET /quizes/:id/answer
 
 exports.answer = function(req, res) {
-models.Quiz.find(req.params.quizId).success(function(quiz){
+models.Quiz.find(req.params.quizId).then(function(quiz){
 	if (req.query.respuesta === quiz.respuesta){
 		res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto'});
         } else {
 		res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
-        }
-})
+        };
+});
 };
